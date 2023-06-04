a0543d1ecbccdaa5d28bcb522cba8ba1b526b087
RXProgrammer <135478308+RXProgrammer@users.noreply.github.com>
2023-06-04 14:35:13 -0400

    Up

--- javascript/script.js
+++ javascript/script.js
@@ -3,12 +3,12 @@ var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.
 
 // Verificar el tamaño de la pantalla para excluir tablets pequeñas
 var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-var isSmallTablet = screenWidth < 768; // Cambia el valor según tu criterio
+var isSmallTablet = screenWidth >= 374 && screenWidth <= 767; // Rango de dimensiones para dispositivos móviles
 
-// Verificar si el usuario actual es tú (desde tu dispositivo móvil)
-// var isYou = /Mozilla\/5.0 \(Linux; Android 10; K\) AppleWebKit\/537.36 \(KHTML, like Gecko\) Chrome\/114.0.0.0 Mobile Safari\/537.36/.test(navigator.userAgent); // Reemplaza con tu agente de usuario
+// Verificar si el usuario actual eres tú (desde tu dispositivo móvil)
+var isYou = /Mozilla\/5.0 \(Linux; Android 10; K\) AppleWebKit\/537.36 \(KHTML, like Gecko\) Chrome\/114.0.0.0 Mobile Safari\/537.36/.test(navigator.userAgent); // Reemplaza con tu agente de usuario
 
 // Redirigir a la página adecuada si se cumple alguna de las condiciones
 if ((isMobile || isSmallTablet) && !isYou) {
-    window.location.href = "https://rxprogrammer.github.io/rxshop/javascript/mantenimiento.html";
+  window.location.href = "https://rxprogrammer.github.io/rxshop/javascript/mantenimiento.html";
 }
