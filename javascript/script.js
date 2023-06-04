9f09bb753b901b90e670a9cfd068200b7120276a
RXProgrammer <135478308+RXProgrammer@users.noreply.github.com>
2023-06-04 14:06:36 -0400

    Update

--- javascript/script.js
+++ javascript/script.js
@@ -6,7 +6,7 @@ var screenWidth = window.innerWidth || document.documentElement.clientWidth || d
 var isSmallTablet = screenWidth < 768; // Cambia el valor según tu criterio
 
 // Verificar si el usuario actual es tú (desde tu dispositivo móvil)
-var isYou = /Mozilla\/5.0 \(Linux; Android 10; K\) AppleWebKit\/537.36 \(KHTML, like Gecko\) Chrome\/114.0.0.0 Mobile Safari\/537.36/.test(navigator.userAgent); // Reemplaza con tu agente de usuario
+// var isYou = /Mozilla\/5.0 \(Linux; Android 10; K\) AppleWebKit\/537.36 \(KHTML, like Gecko\) Chrome\/114.0.0.0 Mobile Safari\/537.36/.test(navigator.userAgent); // Reemplaza con tu agente de usuario
 
 // Redirigir a la página adecuada si se cumple alguna de las condiciones
 if ((isMobile || isSmallTablet) && !isYou) {
