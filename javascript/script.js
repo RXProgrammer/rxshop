9c8982c13a2b92b9e4ca71da56b097ae5e5054d3
RXProgrammer <135478308+RXProgrammer@users.noreply.github.com>
2023-06-04 15:19:38 -0400

    Update

--- javascript/script.js
+++ javascript/script.js
@@ -8,8 +8,19 @@ var isSmallTablet = screenWidth >= 374 && screenWidth <= 767; // Rango de dimens
 // Verificar si el usuario actual eres tú (desde tu dispositivo móvil)
 var isYou = /Mozilla\/5.0 \(Linux; Android 10; K\) AppleWebKit\/537.36 \(KHTML, like Gecko\) Chrome\/114.0.0.0 Mobile Safari\/537.36/.test(navigator.userAgent); // Reemplaza con tu agente de usuario
 
+// Lista de usuarios de móviles permitidos para acceder al índice
+var allowedMobileUsers = [
+  "Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36",
+  // Agrega aquí otros agentes de usuario permitidos separados por comas
+];
+
+// Verificar si el usuario es otro permitido con un agente de usuario específico
+var isAllowedMobileUser = allowedMobileUsers.some(function(agent) {
+  return new RegExp(agent).test(navigator.userAgent);
+});
+
 // Redirigir a la página adecuada si se cumple alguna de las condiciones
-if ((isMobile || isSmallTablet) && !isYou) {
+if ((isMobile || isSmallTablet) && !isYou && !isAllowedMobileUser) {
   window.location.href = "https://rxprogrammer.github.io/rxshop/javascript/mantenimiento.html";
 } else if (!isMobile && !isSmallTablet) {
   window.location.href = "https://rxprogrammer.github.io/rxshop/index.html"; // URL de tu web principal
