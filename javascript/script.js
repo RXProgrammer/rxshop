bafd8c2e06f92366332b1ad696af43ed36cad9d7
RXProgrammer <135478308+RXProgrammer@users.noreply.github.com>
2023-06-04 13:14:35 -0400

    Se agrego el menú de hamburguesa
    Ahora con mayor facilidad se le puede navegador a los usuario por la web.

--- javascript/script.js
+++ javascript/script.js
@@ -7,5 +7,5 @@ var isSmallTablet = screenWidth < 768; // Cambia el valor según tu criterio
 
 // Redirigir a la página adecuada si se cumple alguna de las condiciones
 if (isMobile || isSmallTablet) {
-    window.location.href = "https://tupagina.com/pagina-de-redireccion";
+    window.location.href = "/mantenimiento.html";
 }
